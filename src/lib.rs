36bd846979a2bbcb9529cf4990b8a9e8f01eefe2
junhao chen <84652291+chenjh2002@users.noreply.github.com>
2024-02-27 10:21:11 +0800

    feat(submodule): accomplish sub-module part practice

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,84 @@
-mod front_of_house {
-    mod hosting {
-        fn add_to_wait_list() {}
-        fn seat_at_table() {}
+// mod front_of_house {
+//     pub mod hosting {
+//         pub fn add_to_wait_list() {}
+//         fn seat_at_table() {}
+//     }
+//
+//     mod serving {
+//         fn take_order() {}
+//
+//         fn serve_order() {}
+//
+//         fn take_payment() {}
+//     }
+// }
+
+mod front_of_house;
+
+pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    // // Absolute path
+    // crate::front_of_house::hosting::add_to_wait_list();
+    //
+    // Relative path
+    front_of_house::hosting::add_to_wait_list();
+
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // let order1 = back_of_house::Appetizer::Soup;
+    // let order2 = back_of_house::Appetizer::Salad;
+}
+
+fn deliver_order() {}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
     }
 
-    mod serving {
-        fn take_order() {}
+    fn cook_order() {}
 
-        fn serve_order() {}
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
 
-        fn take_payment() {}
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    impl Appetizer {
+        pub fn choice(choice: &Appetizer) {
+            match choice {
+                Appetizer::Salad => println!("Salad!"),
+                Appetizer::Soup => println!("Soup!"),
+                other=> panic!("Error type"),
+            }
+        }
+    }
+}
+
+// Bringing Paths into Scope with the use Keyword
+mod customer {
+    use crate::front_of_house::hosting;
+
+    pub fn eat_at_restaurant() {
+        hosting::add_to_wait_list();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
+// use std::collections::HashMap;
 use crate::garden::vegetables::Asparagus;
+// use rand::Rng;
+// use std::{self, cmp::Ordering, io};
+// use std::collections::*;
+// use std::io::Result as IoResult;
 
 pub mod garden;
 
 fn main() {
     let plant = Asparagus {};
     println!("I'm growing {:?}!", plant);
+
+    // let mut map = HashMap::new();
+    // map.insert(1, 2);
+    //
+    // let secret_number = rand::thread_rng().gen_range(1..=100);
 }
